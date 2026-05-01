67c139fe74da2d2a93d4f284071fb56c1795fb31
rajeshgone <130182299+Rajeshgone@users.noreply.github.com>
2026-05-01 13:24:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  other: {
+    'base:app_id': '69f459c491ab4dbfa9e7cc50',
+  },
+};
+
+export default function Home() {
+  return (<div>{/* Your page content */}</div>);
+}
 const nextConfig = {
   reactStrictMode: true,
 }
