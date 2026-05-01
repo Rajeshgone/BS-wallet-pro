9da9eb24720f53bd87557314462ffca6565df59d
rajeshgone <130182299+Rajeshgone@users.noreply.github.com>
2026-05-01 12:49:44 +0530

    Create tokens.ts

---
+++ lib/tokens.ts
@@ -0,0 +1,14 @@
+export const TOKENS = [
+  {
+    symbol: "ETH",
+    address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    decimals: 18,
+    icon: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+  },
+  {
+    symbol: "USDC",
+    address: "0xd9aaec86b65d86f6a7b5c1c42ffa531710b6cae9",
+    decimals: 6,
+    icon: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png"
+  }
+]
